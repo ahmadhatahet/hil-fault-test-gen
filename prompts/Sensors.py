3aa8b23d4f298ff3590e6af39fb6dd49647cd023
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2024-07-01 14:11:50 +0200

    add Sensors data

---
+++ prompts/Sensors.py
@@ -0,0 +1,8 @@
+Sensors="""Acceleration Pedal (Acc), vector = [1, 0, 0, 0, 0, 0]
+Wheel Steering Angle (WSA), vector = [0, 1, 0, 0, 0, 0]
+Wheel Speed (WS), vector = [0, 0, 1, 0, 0, 0]
+Yaw Rate (YR), vector = [0, 0, 0, 1, 0, 0]
+Steering Torque (ST), vector = [0, 0, 0, 0, 1, 0]
+Vehicle Speed (VS), vector = [0, 0, 0, 0, 0, 1]
+Acceleration Pedal and Wheel Steering Angle (Acc and WSA), vector = [1, 1, 0, 0, 0, 0]
+Steering Angle and Wheel Speed (WSA and WS), vector = [0, 1, 1, 0, 0, 0]"""
