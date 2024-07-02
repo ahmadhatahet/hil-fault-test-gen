843877d338311f2d0207fd259bcf2310cefd3428
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2024-07-02 14:46:17 +0200

    support multi requirement at once + export accuracy

---
+++ prompts/UserPromptBulk.py
@@ -0,0 +1,5 @@
+UserPromptBulk="""Here is the requirement texts:
+{req}
+
+Return only the vector based on what you learned and each vector following the id number of the requirement it solves.
+"""
