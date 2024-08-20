a1b660ec4145ad1c6caf36df969cf6aa6321df23
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2024-08-20 12:24:17 +0200

    remove size vector for generalization

--- prompts/Sensors.py
+++ prompts/Sensors.py
@@ -1,5 +1,5 @@
-Sensors="""Accelerator Pedal (Acc): Measures the amount of pressure applied to the accelerator pedal, indicating the driver's desired acceleration, vector = [1, 0, 0, 0, 0, 0].
-Wheel Steering Angle (WSA): Measures the angle at which the steering wheel is turned, determining the direction in which the vehicle is being steered, vector = [0, 1, 0, 0, 0, 0].
-Wheel Speed (WS): Measures the rotational speed of the vehicle's wheels, providing information on the vehicle's speed and potential wheel slippage, vector = [0, 0, 1, 0, 0, 0].
-Yaw Rate (YR): Measures the rate of rotation around the vertical axis of the vehicle, indicating its turning behavior and stability, vector = [0, 0, 0, 1, 0, 0].
-Steering Torque (ST): Measures the amount of force applied to the steering wheel, providing feedback on the driver's steering input and the vehicle's response, vector = [0, 0, 0, 0, 1, 0]."""
+Sensors="""Accelerator Pedal (Acc): Measures the amount of pressure applied to the accelerator pedal, indicating the driver's desired acceleration.
+Wheel Steering Angle (WSA): Measures the angle at which the steering wheel is turned, determining the direction in which the vehicle is being steered.
+Wheel Speed (WS): Measures the rotational speed of the vehicle's wheels, providing information on the vehicle's speed and potential wheel slippage.
+Yaw Rate (YR): Measures the rate of rotation around the vertical axis of the vehicle, indicating its turning behavior and stability.
+Steering Torque (ST): Measures the amount of force applied to the steering wheel, providing feedback on the driver's steering input and the vehicle's response."""
