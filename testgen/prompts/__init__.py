5a48b8ff8ad5dde9ae7b47587b981570e5939920
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2024-10-14 13:07:49 +0200

    change to py package structure

---
+++ testgen/prompts/__init__.py
@@ -0,0 +1,10 @@
+from prompts.Sensors import Sensors
+from testgen.prompts.SystemPrompt import SystemPrompt
+from prompts.UserPrompt import UserPrompt
+from prompts.UserPromptBulk import UserPromptBulk
+
+
+Sensors = Sensors
+SystemPrompt = SystemPrompt
+UserPrompt = UserPrompt
+UserPromptBulk = UserPromptBulk
