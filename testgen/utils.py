ca6268d7c4afde2eb4cfb9470a0daddbc199c4dd
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2024-10-27 10:26:54 +0100

    add functions to support openai api

--- testgen/utils.py
+++ testgen/utils.py
@@ -1,7 +1,7 @@
 import re
 import numpy as np
 import time
-
+from openai import AzureOpenAI, OpenAI
 
 def get_examples_from_df(df, n_examples):
 
@@ -75,8 +75,77 @@ def invoke_instance(llm, df, SystemPrompt, Sensors, examples_txt, UserPrompt,ins
 
     return result, response
 
+
 def parse_result(res):
     '''Parse the LLM result'''
     pattern = r"\[(.*?)\]"
     vec = re.findall(pattern, res)[0]
     return f"[{vec}]".replace(" ", "")
+
+
+def openai_client(endpoint, api_key, azure_endpoint="", api_version=""):
+    """
+    Return OpenAI or Azure client for inference
+
+    Args:
+        endpoint (str): "openai" or "azure"
+        api_key (str): API Key
+        azure_endpoint (str): azure endpoint link. Defaults to "".
+        api_version (str): azure endpoint api version. Defaults to "".
+
+    Returns:
+        client: client for inference
+    """
+
+    if endpoint == "azure":
+        return AzureOpenAI(
+            api_key=api_key,
+            azure_endpoint=azure_endpoint,
+            api_version=api_version,
+        )
+
+    if endpoint == "openai":
+        return OpenAI(api_key=api_key)
+
+def client_invoke(client, model_name, SystemPrompt, Sensors, examples_txt, UserPrompt,instance):
+    """Call client for chat completion"""
+
+    # system prompt
+    messages = [
+        {'role': 'system',
+        'content': SystemPrompt.format(sensors=Sensors,examples=examples_txt)}
+    ]
+
+    result = {}
+
+    result["idx"] = instance[0]
+    result["requirement"] = instance[1].iloc[0]
+    result["true_vector"] = "[" + ",".join(map(str, instance[1].iloc[1:])) + "]"
+
+
+    # add user prompt
+    messages.append({"role":"user", "content":UserPrompt.format(req=result["requirement"])})
+
+    # run LLM
+    start_time = time.perf_counter()
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=messages
+    )
+
+    response_time = round(time.perf_counter() - start_time, 6)
+    result["ai_response"] = response.choices[0].message.content
+    result["pred_vector"] = parse_result(result["ai_response"])
+    result["response_time"] = response_time
+
+    result["accuracy"] = result["pred_vector"] == result["true_vector"]
+
+    result.update(response.usage.to_dict())
+
+    # logprobs = response.choices[0].logprobs
+    # message = response.choices[0].message
+    # message = response.choices[0].message
+    # content_filter_results = response.choices[0].content_filter_results
+
+    return result, response
