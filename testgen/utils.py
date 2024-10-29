03c3ce6e2eef78084b078c805e61fad012f412ac
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2024-10-29 09:39:43 +0100

    move bulk functions to utils

--- testgen/utils.py
+++ testgen/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 from openai import AzureOpenAI, OpenAI
 
+
 def get_examples_from_df(df, n_examples):
 
     # collect used examples to exclude from test dataset
@@ -12,9 +13,12 @@ def get_examples_from_df(df, n_examples):
     examples = {}
 
     for c in df.columns[1:]:
-        if df[c].sum() == 0: continue
+        if df[c].sum() == 0:
+            continue
         examples[c] = []
-        i = df.loc[ (df[c] == 1) & (df[df.columns[1:][df.columns[1:]!=c]].sum(axis=1) == 0) ].sample(n_examples)
+        i = df.loc[
+            (df[c] == 1) & (df[df.columns[1:][df.columns[1:] != c]].sum(axis=1) == 0)
+        ].sample(n_examples)
         indexes_to_drop.append(i.index)
         for e in i.values:
             examples[c].append([e[0], "[" + ",".join(map(str, e[1:])) + "]"])
@@ -28,7 +32,7 @@ def get_examples_from_df(df, n_examples):
     t = df.iloc[idx, :].values
 
     for r in t:
-        c = "&".join(df.columns[1:][r[1:]==1])
+        c = "&".join(df.columns[1:][r[1:] == 1])
         if examples_multiple.get(c) is None:
             examples_multiple[c] = []
         examples_multiple[c].append([r[0], "[" + ",".join(map(str, r[1:])) + "]"])
@@ -36,14 +40,13 @@ def get_examples_from_df(df, n_examples):
     # add both single and multiple into one place
     examples.update(examples_multiple)
 
-
     indexes_to_drop = np.array(indexes_to_drop).flatten()
 
     return indexes_to_drop, examples
 
 
 def parse_result(res):
-    '''Parse the LLM result'''
+    """Parse the LLM result"""
     pattern = r"\[(.*?)\]"
     vec = re.findall(pattern, res)[0]
     return f"[{vec}]".replace(" ", "")
@@ -73,13 +76,25 @@ def openai_client(endpoint, api_key, azure_endpoint="", api_version=""):
     if endpoint == "openai":
         return OpenAI(api_key=api_key)
 
-def client_invoke(client, model_name, temperature, SystemPrompt, Sensors, examples_txt, UserPrompt,instance):
+
+def client_invoke(
+    client,
+    model_name,
+    temperature,
+    SystemPrompt,
+    Sensors,
+    examples_txt,
+    UserPrompt,
+    instance,
+):
     """Call client for chat completion"""
 
     # system prompt
     messages = [
-        {'role': 'system',
-        'content': SystemPrompt.format(sensors=Sensors,examples=examples_txt)}
+        {
+            "role": "system",
+            "content": SystemPrompt.format(sensors=Sensors, examples=examples_txt),
+        }
     ]
 
     result = {}
@@ -88,9 +103,10 @@ def client_invoke(client, model_name, temperature, SystemPrompt, Sensors, exampl
     result["requirement"] = instance[1].iloc[0]
     result["true_vector"] = "[" + ",".join(map(str, instance[1].iloc[1:])) + "]"
 
-
     # add user prompt
-    messages.append({"role":"user", "content":UserPrompt.format(req=result["requirement"])})
+    messages.append(
+        {"role": "user", "content": UserPrompt.format(req=result["requirement"])}
+    )
 
     # run LLM
     start_time = time.perf_counter()
@@ -116,3 +132,93 @@ def client_invoke(client, model_name, temperature, SystemPrompt, Sensors, exampl
     # content_filter_results = response.choices[0].content_filter_results
 
     return result, response
+
+
+def get_batches(df, SAMPLE_TYPE="random", N_REQS=5):
+    batches = []
+
+    while df.shape[0] > N_REQS:
+        if SAMPLE_TYPE == "random":
+            instances = df.sample(N_REQS)
+            df.drop(index=instances.index, inplace=True)
+        else:
+            instances = df.iloc[:N_REQS, :]
+            df.drop(index=instances.index, inplace=True)
+
+        batches.append(instances)
+
+    print("Number of Batches:", len(batches))
+    print("Number of Instances left:", df.shape[0])
+
+    return batches
+
+
+def requirement_text_bulk(batches):
+    # split and format batches
+    for i, batch in enumerate(batches):
+        idx = batch.index.to_list()
+        req = batch["requirement"].tolist()
+        vec = (
+            batch.iloc[:, 1:]
+            .apply(lambda x: "[" + ",".join(map(str, x)) + "]", axis=1)
+            .to_list()
+        )
+        batches[i] = (idx, req, vec)
+
+    # requirement text
+    req_texts = []
+    for i, batch in enumerate(batches):
+        req_text = ""
+        for i, r in enumerate(batch[1]):
+            req_text += f"Requirement {i+1}: {r}.\n"
+        req_texts.append(req_text)
+
+    return batches, req_texts
+
+
+def invoke_bulk(
+    model_name,
+    temperature,
+    client,
+    batch,
+    SystemPrompt,
+    Sensors,
+    examples_txt,
+    UserPromptBulk,
+    req_text,
+):
+    result = {}
+
+    messages = [
+        {
+            "role": "system",
+            "content": SystemPrompt.format(sensors=Sensors, examples=examples_txt),
+        }
+    ]
+
+    # add user prompt
+    messages.append({"role": "user", "content": UserPromptBulk.format(req=req_text)})
+
+    start_time = time.perf_counter()
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        temperature=temperature,
+    )
+    response_time = round(time.perf_counter() - start_time, 6)
+
+    vectors = [parse_result(v) for v in response.choices[0].message.content.split("\n")]
+    accuracy = [True if gt == v else False for gt, v in zip(batch[2], vectors)]
+
+    result["idx"] = batch[0]
+    result["requirement"] = batch[1]
+    result["true_vector"] = batch[2]
+    result["ai_response"] = response.choices[0].message.content
+    result["pred_vector"] = vectors
+    result["response_time"] = response_time
+
+    result["accuracy"] = accuracy
+
+    result.update(response.usage.to_dict())
+
+    return result
